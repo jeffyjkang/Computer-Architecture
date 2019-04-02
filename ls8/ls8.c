577d03aab3d55b9edcdb125c1d47c3db8aefd434
jeffyjkang <jeffyjkang@gmail.com>
2019-04-02 13:17:04 -0700

    main update

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -4,12 +4,22 @@
 /**
  * Main
  */
-int main(void)
+// argc is argument count, argv is array of strings that hold individual arguments
+int main(int argc, char *argv[])
 {
   struct cpu cpu;
 
+  // if arggument count is less than 2
+  if (argc < 2)
+  {
+    // print error
+    printf("Error, no argument given for filename\n");
+    exit(1);
+  }
+
   cpu_init(&cpu);
-  cpu_load(&cpu);
+  // change cpu_load, to have second param, pass in argument string at index 1
+  cpu_load(&cpu, argv[1]);
   cpu_run(&cpu);
 
   return 0;
